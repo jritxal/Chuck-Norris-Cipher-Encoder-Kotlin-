332786ee9cdad4c497b1e0210097d1dada76b746
jritxal <javicionax@hotmail.com>
2024-01-09 08:25:31 +0100

    problem solved

---
+++ Topics/Declaring functions/The square of a number/src/Main.kt
@@ -0,0 +1,11 @@
+// write your code here
+
+/* Do not change code below */
+fun main() {
+    val number = readLine()!!.toInt()
+    println(square(number))
+}
+
+fun square(n: Int): Int {
+    return n * n
+}
